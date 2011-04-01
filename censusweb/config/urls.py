4138e52c8e23c6b5787a0e24f32c1c8aa6627e64
Jeremy Ashkenas <jashkenas@gmail.com>
2011-04-01 13:20:47 -0500

    removing default regexes

--- censusweb/config/urls.py
+++ censusweb/config/urls.py
@@ -6,14 +6,7 @@ from datathing import views
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^simple_project/', include('simple_project.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
-    
     (r'^datathing\.(?P<extension>(json)|(csv))', views.theapi)
+
 )
