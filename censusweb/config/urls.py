2b3808c4ba24ca7d6da97ed292ecd7074f3c8319
Brian Boyer <bboyer@tribune.com>
2011-04-01 17:58:23 -0500

    refactored urls in to api app

--- censusweb/config/urls.py
+++ censusweb/config/urls.py
@@ -1,24 +1,7 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-from api import views
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
-
-    # The Homepage.
-    (r'^$', views.homepage),
-    
-    # Tracts
-    #/tracts/illinois.json
-    #/tracts/illinois/cook.json
-    #/tracts/illinois/cook/00001.json
-    
-    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT
-    }),
-    
-    (r'tracts/(?P<state>[A-Z]{2})(/(?P<county>\d{3}))?(/(?P<tract>\d{5}))?\.(?P<extension>json|csv|html)$', views.tracts)
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }),
+    (r'^', include('api.urls')),
 )
