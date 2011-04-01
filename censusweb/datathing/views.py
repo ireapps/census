a818eb429bbccd2577e71584ce0e1986c5e6c31b
Jeremy Ashkenas <jashkenas@gmail.com>
2011-04-01 14:50:32 -0500

    homepage

--- censusweb/datathing/views.py
+++ censusweb/datathing/views.py
@@ -5,3 +5,6 @@ from django.template import RequestContext
 
 def theapi(request, extension):
     return render_to_response('datathing.html', {'extension': extension}, context_instance=RequestContext(request))
+
+def homepage(request):
+    return render_to_response('homepage.html', context_instance=RequestContext(request))
