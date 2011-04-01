61c6ce4baeb0e8a8f141d9b15685248b1beb7b7c
Brian Boyer <bboyer@tribune.com>
2011-04-01 16:28:00 -0500

    got some css in there

--- censusweb/config/urls.py
+++ censusweb/config/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 from datathing import views
 
 # Uncomment the next two lines to enable the admin:
@@ -14,5 +15,10 @@ urlpatterns = patterns('',
     #/tracts/illinois.json
     #/tracts/illinois/cook.json
     #/tracts/illinois/cook/00001.json
+    
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT
+    }),
+    
     (r'tracts/(?P<state>[A-Z]{2})(/(?P<county>\d{3}))?(/(?P<tract>\d{5}))?\.(?P<extension>json|csv|html)$', views.tracts)
 )
