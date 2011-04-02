0eb186746a1721809215813eb812e4e45673f428
Aron Pilhofer <aron@Aron-Pilhofers-MacBook-Air.local>
2011-04-01 19:51:10 -0500

    parallel downloader

--- import/downloader.py
+++ import/downloader.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from lib import STATES
+from multiprocessing import Pool
 import os
 
 cmd = "python census2text.py -q --state '%s' --geography tract  -w -o data/%s.tsv %s "
@@ -9,7 +10,13 @@ try:
     os.mkdir('data')
 except OSError: pass
 
-for state, number, postal in STATES:
+def download_state(params):
+    state, number, postal = params
+    print "downloading %s" % state
     command = cmd % (state, number, ' '.join(TABLES))
     os.system( command )
     print "downloaded %s" % state
+
+p = Pool(5)
+p.map(download_state, STATES)
+    
