ca5bf0da591a74e38b2a6d1bfca62eb00e28ef49
Aron Pilhofer <aron@Aron-Pilhofers-MacBook-Air.local>
2011-04-03 10:39:55 -0500

    less smelly code

--- import/data_importer.py
+++ import/data_importer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # from data_importer import DataImporter 
-# o = DataImporter()
+# 
+
 import psycopg2
 import csv
 import urllib
@@ -15,6 +16,7 @@ class DataImporter:
         self.conn = psycopg2.connect(DB_CONNECT_STRING)
         self.cur = self.conn.cursor()
         self.table_name = geo_level + "_data_2000"
+        self.files = filter(lambda x: x.endswith(".tsv"), listdir("./data"))
         
     def import_data(self):
         self._drop_old_tables()        
@@ -32,18 +34,17 @@ class DataImporter:
         create_sql = "create table %s (%s)" % (self.table_name, ",".join(fields_and_values["fields"]))
         self.cur.execute(create_sql)
         self.conn.commit()
-    
+        
     def _insert_data(self,fields_and_values):
         insert_sql = "insert into " + self.table_name + " (" + ",".join(fields_and_values['headers']) + ")"
-        for file in filter(lambda x: x.endswith(".tsv"), listdir("./data")):
-            cur_file = "Working on file %s" % file
+        for file in self.files:
             reader = csv.reader(open(os.path.join("./data",file)), delimiter='\t')
             header = reader.next()
             for i,line in enumerate(reader):
                 values = ",".join(self._string_or_num(fields_and_values, line))
                 self.cur.execute(insert_sql + " values (" + values + ")")
                 self.conn.commit()
-                print cur_file + "\tline %s" % str(i)
+                print "Working on file %s" % file + "\t: Line %s" % str(i)
         
     def _string_or_num(self,fields_and_values,line):
         results = []
@@ -55,31 +56,28 @@ class DataImporter:
         return results
             
     def _get_file_heads(self):
-        file_name = filter(lambda x: x.endswith(".tsv"), listdir("./data"))
-        reader = csv.reader(open(os.path.join("./data",file_name[0])), delimiter='\t')
+        reader = csv.reader(open(os.path.join("./data", self.files[0])), delimiter='\t')
         header_line = reader.next()
         data_line = reader.next()
         return {"fields":[], "headers":header_line, "data_line":data_line, "types":[]}
             
     def _tablize_headers(self, data_and_headers):
-        special_headers = ["Summary Level", "Geographic Component", "State FIPS", "Place FIPS", "County FIPS", "Tract", "Zip", "Block", "Name"]
         for i,node in enumerate(data_and_headers["data_line"]):
-            header = data_and_headers["headers"][i]
-            data_and_headers["headers"][i] = header.replace(" ", "_")
-            if header in special_headers:
-                print header + " yes"
-                data_and_headers["fields"].append(header.replace(" ", "_") + " VARCHAR(255)")
-                data_and_headers["types"].append("s")
-            elif re.search('^-?\d+\.\d+$', node):
-                data_and_headers["fields"].append(header.replace(" ", "_") + " NUMERIC(11,6)")
-                data_and_headers["types"].append("n")
-            elif node.isdigit() and not re.search('^0', node):
-                data_and_headers["fields"].append(header.replace(" ", "_") + " INTEGER")
-                data_and_headers["types"].append("i")
-            else:
-                data_and_headers["fields"].append(header.replace(" ", "_") + " VARCHAR(255)")
-                data_and_headers["types"].append("s")
+            datatype = self._of_type(node, data_and_headers["headers"][i])
+            data_and_headers["fields"].append(header.replace(" ", "_") + datatype[1])
+            data_and_headers["types"].append(datatype[0])
         return data_and_headers
+       
+    def _of_type(self,node,header):
+        SPECIAL_HEADERS = ["Summary Level", "Geographic Component", "State FIPS", "Place FIPS", "County FIPS", "Tract", "Zip", "Block", "Name"]
+        if header in SPECIAL_HEADERS:
+            return ("s"," VARCHAR(255)")
+        elif re.search('^-?\d+\.\d+$', node):
+            return ("n"," NUMERIC(11,6)")
+        elif node.isdigit() and not re.search('^0', node):
+            return ("i"," INTEGER")
+        else:
+            return ("s"," VARCHAR(255)")
             
     def _drop_old_tables(self):
         self.cur.execute("drop table if exists %s" % self.table_name)
