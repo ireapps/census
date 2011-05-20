021477fb655060a755dc973f69f0f7fc56729948
cgroskopf <cgroskopf@tribune.com>
2011-05-20 12:04:35 -0500

    Parametized sf loader, added FAKE option for using 2000 data as 2010.

--- dataprocessing/batch_sf.sh
+++ dataprocessing/batch_sf.sh
@@ -2,12 +2,31 @@
 
 # See batch.sh for notes.
 
-#echo "use census; 
-#db.dropDatabase();" | mongo
+echo "use census; 
+db.geographies.ensureIndex({ 'geoid': 1 });
+db.geographies.ensureIndex({ 'metadata.STATE': 1 });
+db.geographies.ensureIndex({ 'xrefs': 1 });" | mongo
 
-./fetch_sf_data.sh
+STATE_NAME=$1
+STATE_NAME_LOWER=`echo $1 | tr '[A-Z]' '[a-z]'`
+STATE_NAME_ABBR=`python get_state_abbr.py $1`
+STATE_FIPS=`python get_state_fips.py $1`
+FAKE=$2
 
-./load_sf_geographies_2000.py data/degeo2000.csv
-./load_sf_data_2000.py data/sf_data_2000_delaware_1.csv
+./fetch_sf_data.sh $STATE_NAME $STATE_NAME_LOWER $STATE_NAME_ABBR $FAKE
 
-#./load_pl_labels_2010.py data/pl_2010_data_labels.csv
+./load_sf_geographies_2000.py data/${STATE_NAME_ABBR}geo2000.csv
+./load_sf_data_2000.py data/sf_data_2000_${STATE_NAME_LOWER}_1.csv
+
+# TODO
+#./load_sf_labels_2010.py data/sf_2010_data_labels.csv
+
+# Load 2000 data as 2010 for testing
+if [ "$FAKE" = "FAKE" ]; then
+    ./load_sf_geographies_2010.py data/${STATE_NAME_ABBR}geo2010.csv
+    ./load_crosswalk.py $STATE_FIPS $FAKE 
+    ./load_sf_data_2010.py data/sf_data_2010_${STATE_NAME_LOWER}_1.csv
+
+    ./crosswalk.py $STATE_FIPS
+    ./compute_deltas.py $STATE_FIPS
+fi
--- dataprocessing/fetch_sf_data.sh
+++ dataprocessing/fetch_sf_data.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-# NO NATIONAL DATA!!!
+STATE_NAME=$1
+STATE_NAME_LOWER=$2
+STATE_NAME_ABBR=$3
+FAKE=$4
 
 rm -r data
 mkdir data
 cd data
 
-# 2000 - DELAWARE - SF1
-wget http://www2.census.gov/census_2000/datasets/Summary_File_1/Delaware/de00001_uf1.zip
-unzip de00001_uf1.zip
+# 2000
+wget http://www2.census.gov/census_2000/datasets/Summary_File_1/${STATE_NAME}/${STATE_NAME_ABBR}00001_uf1.zip
+unzip ${STATE_NAME_ABBR}00001_uf1.zip
 
-wget http://www2.census.gov/census_2000/datasets/Summary_File_1/Delaware/degeo_uf1.zip
-unzip degeo_uf1.zip
+wget http://www2.census.gov/census_2000/datasets/Summary_File_1/${STATE_NAME}/${STATE_NAME_ABBR}geo_uf1.zip
+unzip ${STATE_NAME_ABBR}geo_uf1.zip
 
 wget http://www.census.gov/support/2000/SF1/Access97.zip
 unzip Access97.zip
 mdb-export SF1.mdb SF10001 > sf_data_2000_headers_1.csv
 mdb-export SF1.mdb TABLES > sf_2000_data_labels.csv
 
-rm sf_data_2000_delaware_1.csv
-cat sf_data_2000_headers_1.csv > sf_data_2000_delaware_1.csv
-cat de00001.uf1 >> sf_data_2000_delaware_1.csv
+rm sf_data_2000_${STATE_NAME_LOWER}_1.csv
+cat sf_data_2000_headers_1.csv > sf_data_2000_${STATE_NAME_LOWER}_1.csv
+cat ${STATE_NAME_ABBR}00001.uf1 >> sf_data_2000_${STATE_NAME_LOWER}_1.csv
 
-in2csv -f fixed -s ../census2000_geo_schema.csv degeo.uf1 > degeo2000.csv
+in2csv -f fixed -s ../census2000_geo_schema.csv ${STATE_NAME_ABBR}geo.uf1 > ${STATE_NAME_ABBR}geo2000.csv
+
+# 2010
+
+# Load 2000 data as 2010 for testing
+if [ "$FAKE" = "FAKE" ]; then
+    cp sf_data_2000_${STATE_NAME_ABBR}_1.csv sf_data_2010_${STATE_NAME_ABBR}_1.csv
+    cp ${STATE_NAME_ABBR}geo2000.csv ${STATE_NAME_ABBR}geo2010.csv
+fi
+
