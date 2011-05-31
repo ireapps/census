ff996a110c1f0e901f4116ca9a1adfd9b1b401a0
Joe Germuska <joe@germuska.com>
2011-05-31 16:18:59 -0500

    add a bootstrap process for geodata

---
+++ censusweb/fetch_geodata.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DATA_DIR=$1
+STATE_FIPS_CODES=${@:2}
+
+start_dir=`pwd`
+
+for STATE_FIPS_CODE in $STATE_FIPS_CODES
+do
+    mkdir -p "${DATA_DIR}/STATE"
+    cd "${DATA_DIR}/STATE"
+    wget "ftp://ftp2.census.gov/geo/tiger/TIGER2010/STATE/2010/tl_2010_${STATE_FIPS_CODE}_state10.zip"
+
+    cd $start_dir
+
+    mkdir -p "${DATA_DIR}/COUNTY"
+    cd "${DATA_DIR}/COUNTY"
+    wget "ftp://ftp2.census.gov/geo/tiger/TIGER2010/COUNTY/2010/tl_2010_${STATE_FIPS_CODE}_county10.zip"
+
+    cd $start_dir
+
+    mkdir -p "${DATA_DIR}/PLACE"
+    cd "${DATA_DIR}/PLACE"
+    wget "ftp://ftp2.census.gov/geo/tiger/TIGER2010/PLACE/2010/tl_2010_${STATE_FIPS_CODE}_place10.zip"
+
+    cd $start_dir
+
+    mkdir -p "${DATA_DIR}/TRACT"
+    cd "${DATA_DIR}/TRACT"
+    wget "ftp://ftp2.census.gov/geo/tiger/TIGER2010/TRACT/2010/tl_2010_${STATE_FIPS_CODE}_tract10.zip"
+
+    cd $start_dir
+done
+echo "Done"
+
