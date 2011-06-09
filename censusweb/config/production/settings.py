e99e29a569b38070a0fd641c46863cda6a7de876
cgroskopf <cgroskopf@tribune.com>
2011-06-09 15:33:14 -0500

    Updated Apache and Django config with production domains.

--- censusweb/config/production/settings.py
+++ censusweb/config/production/settings.py
@@ -4,7 +4,7 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 # Database
-DATABASES['default']['HOST'] = 'censusweb.censusweb.ire.org'
+DATABASES['default']['HOST'] = 'census.ire.org'
 DATABASES['default']['PORT'] = '5433'
 DATABASES['default']['USER'] = 'censusweb'
 DATABASES['default']['PASSWORD'] = 'Xy9XKembdu'
@@ -12,15 +12,15 @@ DATABASES['default']['PASSWORD'] = 'Xy9XKembdu'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://media.censusweb.ire.org/censusweb/site_media/'
+MEDIA_URL = 'http://media.census.ire.org/site_media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = 'http://media.censusweb.ire.org/censusweb/admin_media/'
+ADMIN_MEDIA_PREFIX = 'http://media.census.ire.org/admin_media/'
 
 # Predefined domain
-MY_SITE_DOMAIN = 'censusweb.censusweb.ire.org'
+MY_SITE_DOMAIN = 'census.ire.org'
 
 # Email
 EMAIL_HOST = 'mail'
@@ -30,7 +30,10 @@ EMAIL_PORT = 25
 CACHE_BACKEND = 'memcached://cache:11211/'
 
 # S3
-AWS_S3_URL = 's3://media.censusweb.ire.org/censusweb/'
+AWS_S3_URL = 's3://media.census.ire.org/'
+
+# Application settings
+API_URL = 'http://media.census.ire.org/'
 
 # Internal IPs for security
 INTERNAL_IPS = ()
