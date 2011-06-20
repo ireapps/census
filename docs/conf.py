1df3fc0760c2a564a83e44c32f50b9f426b02158
Curt Merrill <curt.merrill@gmail.com>
2011-06-20 11:15:34 -0400

    updating docs

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'IRE-Census'
-copyright = u'2011, Curt'
+copyright = u'2011, IRE'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -179,7 +179,7 @@ htmlhelp_basename = 'IRE-Censusdoc'
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'IRE-Census.tex', u'IRE-Census Documentation',
-   u'Curt', 'manual'),
+   u'IRE', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -212,5 +212,5 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'ire-census', u'IRE-Census Documentation',
-     [u'Curt'], 1)
+     [u'IRE'], 1)
 ]
