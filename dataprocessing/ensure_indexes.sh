81e8fcde2aa5c744717673e8b3fd9c87e6d67f7c
cgroskopf <cgroskopf@tribune.com>
2011-06-21 08:54:42 -0500

    Added index to enable sorting geographies by name for lookup tables.

--- dataprocessing/ensure_indexes.sh
+++ dataprocessing/ensure_indexes.sh
@@ -3,4 +3,5 @@
 echo "use census; 
 db.geographies.ensureIndex({ 'geoid': 1 });
 db.geographies.ensureIndex({ 'xrefs': 1 });
+db.geographies.ensureIndex({ 'metadata.NAME': 1 });
 db.geographies_2000.ensureIndex({ 'geoid': 1 });" | mongo
