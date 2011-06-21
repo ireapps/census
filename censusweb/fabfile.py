b4ac758488e44f5cfba7369eddc8703970c67cb7
cgroskopf <cgroskopf@tribune.com>
2011-06-21 08:33:50 -0500

    Add latest field mappings.

--- censusweb/fabfile.py
+++ censusweb/fabfile.py
@@ -140,7 +140,6 @@ def deploy_requirements_to_s3():
         run('s3cmd del --recursive s3://%(s3_bucket)s/%(project_name)s/%(admin_media_prefix)s/' % env)
     run('s3cmd -P --guess-mime-type --rexclude-from=%(site_path)s/s3exclude sync %(env_path)s/src/django/django/contrib/admin/media/ s3://%(s3_bucket)s/%(project_name)s/%(admin_media_prefix)s/' % env)
 
-
 """
 Commands - deployment
 """
