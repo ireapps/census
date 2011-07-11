9defb29cee4b34420d65d1a61a1ce158cddbdef2
Joe Germuska <joe@germuska.com>
2011-07-11 13:27:10 -0500

    turn on boundaryservice API

--- censusweb/config/urls.py
+++ censusweb/config/urls.py
@@ -3,5 +3,7 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }),
+#    url(r'^geo/$', views.generic_view, { "template": "geo-documentation.html" }, name="geo-documentation"),
+    (r'^geo/.+', include('boundaryservice.urls')),
     (r'^', include('api.urls')),
 )
