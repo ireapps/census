f3e69c1cfb9a17f8367847149fb762f0b9fba039
Joe Germuska <joe@germuska.com>
2011-07-19 14:25:47 -0500

    add next week's states

---
+++ dataprocessing/batch_20110726.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 16 ID 
+./batch_sf.sh "Idaho" production
+# 54 WV 
+./batch_sf.sh "West Virginia" production
+# 49 UT 
+./batch_sf.sh "Utah" production
+# 55 WI 
+./batch_sf.sh "Wisconsin" production
+# 29 MO 
+./batch_sf.sh "Missouri" production
+# 39 OH 
+./batch_sf.sh "Ohio" production
