8987036eaa0af70b92681fe9f469d3c019b38fa7
Joe Germuska <joe@germuska.com>
2011-07-19 10:11:49 -0500

    turn off block-specific changes

--- dataprocessing/batch_sf.sh
+++ dataprocessing/batch_sf.sh
@@ -22,14 +22,14 @@ echo 'Ensuring mongo indexes.'
 ./ensure_indexes.sh
 
 echo 'Fetching data'
-#./fetch_sf_data_2000.sh "$STATE_NAME_SPACE_FIXED" "$STATE_NAME_LOWER" "$STATE_NAME_ABBR" "$STATE_FIPS"
-#./fetch_sf_data_2010.sh "$STATE_NAME_SPACE_FIXED" "$STATE_NAME_LOWER" "$STATE_NAME_ABBR"
+./fetch_sf_data_2000.sh "$STATE_NAME_SPACE_FIXED" "$STATE_NAME_LOWER" "$STATE_NAME_ABBR" "$STATE_FIPS"
+./fetch_sf_data_2010.sh "$STATE_NAME_SPACE_FIXED" "$STATE_NAME_LOWER" "$STATE_NAME_ABBR"
 
 echo 'Loading 2000 geographies'
 ./load_sf_geographies_2000.py data/${STATE_NAME_ABBR}geo2000.csv || exit $?
 
 echo 'Loading 2000 data'
-for i in {1..1}
+for i in {1..39}
 do
     ./load_sf_data_2000.py data/sf_data_2000_${STATE_NAME_LOWER}_$i.csv 
 done
@@ -56,7 +56,7 @@ else
 fi
 
 echo 'Loading 2010 data'
-for i in {1..1}
+for i in {1..47}
 do
     if [ "$FAKE" = "FAKE" ]; then
         # Load 2000 data as 2010 for testing
