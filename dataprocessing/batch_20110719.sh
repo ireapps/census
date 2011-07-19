bb9fb750386477ead57bb6422bf932a4612352cc
Ubuntu <ubuntu@ip-10-102-27-148.ec2.internal>
2011-07-19 15:12:26 +0000

    add batch scripts

---
+++ dataprocessing/batch_20110719.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 50
+./batch_sf.sh Vermont production
+# 36
+./batch_sf.sh "New York" production
+# 29
+./batch_sf.sh Montana production
+# 40
+./batch_sf.sh Oklahoma production
+# 04
+./batch_sf.sh Arizona production
+# 27
+./batch_sf.sh Minnesota production
+
