b51d02c51e406830b80394488bd8b1b7e1e89e26
Joe Germuska <joe@germuska.com>
2011-07-20 11:10:41 -0500

    oh yeah, handle CLI arg

--- dataprocessing/restore_state.sh
+++ dataprocessing/restore_state.sh
@@ -2,4 +2,6 @@
 # Given a state fips code, restore that state to the mongo DB
 # this REPLACES current data, so use with caution
 MONGO_DUMP_DIR="/mnt/data/mongodumps"
+STATE_FIPS="${@:1:1}"
+
 mongorestore -d census --drop ${MONGO_DUMP_DIR}/${STATE_FIPS}/census
