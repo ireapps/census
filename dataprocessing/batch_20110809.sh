b053ae5e3a97e0d4a7eda471195ffff9ae520bfe
Joe Germuska <joe@germuska.com>
2011-08-08 16:19:08 -0500

    add batch for AR, IA, LA, MD, NJ, TX

---
+++ dataprocessing/batch_20110809.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 05 AR 
+./batch_sf.sh "Arkansas" production
+# 19 IA 
+./batch_sf.sh "Iowa" production
+# 22 LA 
+./batch_sf.sh "Louisiana" production
+# 24 MD 
+./batch_sf.sh "Maryland" production
+# 34 NJ 
+./batch_sf.sh "New Jersey" production
+# 48 TX 
+./batch_sf.sh "Texas" production
