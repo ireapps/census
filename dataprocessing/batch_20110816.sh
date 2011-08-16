d3924df919f3897cf8db31553187f5ba77dbe0c6
Joe Germuska <joe@germuska.com>
2011-08-16 08:58:08 -0500

    add new batch file

---
+++ dataprocessing/batch_20110816.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 11 DC 
+./batch_sf.sh "District of Columbia" production
+# 45 SC 
+./batch_sf.sh "South Carolina" production
+# 25 MA 
+./batch_sf.sh "Massachusetts" production
+# 26 MI 
+./batch_sf.sh "Michigan" production
+# 12 FL 
+./batch_sf.sh "Florida" production
