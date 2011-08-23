1abf00d6e1c3fd5d5ee49e5b0e7615efc6b879d4
Joe Germuska <joe@germuska.com>
2011-08-23 09:51:38 -0500

    last batch load script

---
+++ dataprocessing/batch_20110823.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 38 ND 
+./batch_sf.sh "North Dakota" production
+# 44 RI 
+./batch_sf.sh "Rhode Island" production
+# 35 NM 
+./batch_sf.sh "New Mexico" production
+# 28 MS 
+./batch_sf.sh "Mississippi" production
+# 47 TN 
+./batch_sf.sh "Tennessee" production
